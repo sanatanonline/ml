9c1557b4be5241a5da82f65da4905361931ac3a9
Sanatan Das <sanatanonline@gmail.com>
2022-07-16 18:53:56 -0700

    code updated

--- src/basic/modules.py
+++ src/basic/modules.py
@@ -1,18 +1,18 @@
-data = open('sketch.txt')
-print(type(data))
+try:
+    data = open('sketch.txt')
+    print(type(data))
+    print(data.readline(), end='')
+    data.seek(0)
 
-print(data.readline(), end='')
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(sep=':', maxsplit=3)
+            print(role, end="")
+            print("said", end="")
+            print(line_spoken, end="")
+        except ValueError:
+            pass
 
-data.seek(0)
-
-for each_line in data:
-    print(type(each_line))
-    print(each_line, end='')
-    (role, line_spoken) = each_line.split(':')
-    print(role, end="")
-    print("said", end="")
-    print(line_spoken, end="")
-
-data.close()
-
-
+    data.close()
+except IOError:
+    print("File not found")
