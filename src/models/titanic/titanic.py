a4c45b9abcf293c710e3f451d45d7e59529a68fe
Sanatan Das <sanatanonline@gmail.com>
2022-08-17 23:22:00 -0700

    code updated

--- src/models/titanic/titanic.py
+++ src/models/titanic/titanic.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 training_data = pd.read_csv("/Users/sanatan/local/data/titanic/train_data.csv")
+
 nRow, nCol = training_data.shape
 print(f'There are {nRow} rows and {nCol} columns')
 
-print(training_data.head(5))
+print(training_data.head(5).to_string())
+
+sns.scatterplot(data=training_data, x="Age", y="Fare", hue="Survived")
+plt.show()
+
